bba3c0005b131ee1d20201c4aac7209617eea21d
gaydukov <gaydukov_s@bigmir.net>
2019-02-26 09:32:55 +0200

    manyToOne

--- src/main/java/com/sweater/controller/MainController.java
+++ src/main/java/com/sweater/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.sweater.controller;
 
 import com.sweater.domain.Message;
+import com.sweater.domain.User;
 import com.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,39 +20,48 @@ public class MainController {
     private MessageRepo messageRepo;
 
     @GetMapping("/")
-    public String greeting(Map<String,Object> model) {
+    public String greeting(Map<String, Object> model) {
         return "greeting";
     }
 
-    @GetMapping("main")
-    public String main (Map<String,Object> model){
+    @GetMapping("/main")
+    public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages",messages);
+
+        model.put("messages", messages);
+
         return "main";
     }
 
-    @PostMapping("main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String,Object> model){
-        Message message=new Message(text,tag);
+    @PostMapping("/main")
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
+
         messageRepo.save(message);
 
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages",messages);
+
+        model.put("messages", messages);
+
         return "main";
     }
 
     @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String,Object> model){
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
         Iterable<Message> messages;
 
-        if(filter!=null && !filter.isEmpty())
+        if (filter != null && !filter.isEmpty()) {
             messages = messageRepo.findByTag(filter);
-        else
+        } else {
             messages = messageRepo.findAll();
-
+        }
 
         model.put("messages", messages);
+
         return "main";
     }
-
 }
