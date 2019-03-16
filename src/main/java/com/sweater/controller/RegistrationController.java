85c21687019d2182f32d5ad6122a1a2a6c5bb7be
Александр <gaydukov_s@bigmir.net>
2019-03-16 22:17:46 +0200

    mail sender

--- src/main/java/com/sweater/controller/RegistrationController.java
+++ src/main/java/com/sweater/controller/RegistrationController.java
@@ -1,20 +1,20 @@
 package com.sweater.controller;
 
-import com.sweater.domain.Role;
 import com.sweater.domain.User;
-import com.sweater.repos.UserRepo;
+import com.sweater.service.UserSevice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Collections;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
     @Autowired
-    private UserRepo userRepo;
+    private UserSevice userSevice;
     @GetMapping("/registration")
     public String registration(){
         return "registration";
@@ -22,17 +22,29 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, Map<String, Object> model){
-        User userFromDB=userRepo.findByUsername(user.getUsername());
 
-        if(userFromDB != null){
+
+        if(!userSevice.addUser(user)){
             model.put("message", "User exists!");
             return "registration";
         }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepo.save(user);
+
 
         return "redirect:/login";
 
     }
+
+    @GetMapping("/activate/{code}")
+    public String activate(Model model, @PathVariable String code){
+        boolean isActivated=userSevice.activateUser(code);
+        if(isActivated){
+            model.addAttribute("message","User successfully activated");
+        }else {
+            model.addAttribute("message","Activation code is not found");
+        }
+
+        return "login";
+    }
+
+
 }
